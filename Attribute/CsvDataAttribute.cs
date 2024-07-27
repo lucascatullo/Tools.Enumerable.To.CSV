d44234b961b457bd880b95568ec5a1c701760df0
LucasC <lucas@mymarketplacebuilder.com>
2024-07-27 14:12:57 +0200

    Extract CsvCells using reflection from CsvDataAttribute

---
+++ Attribute/CsvDataAttribute.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Tools.Enumerable.To.CSV.Attribute;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class CsvDataAttribute(string csvName) : System.Attribute
+{
+    private readonly string _csvName = csvName;
+
+    public string GetName() => _csvName;
+
+}
---
+++ Model/CsvCell.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Tools.Enumerable.To.CSV.Attribute;
+
+namespace Tools.Enumerable.To.CSV.Model;
+
+public class CsvCell(string name , string value)
+{
+    public string Name { get; } = name;
+    public string Value { get; } = value;
+
+
+    public static IList<CsvCell> ExtractLines(object myObject)
+    {
+        IList<CsvCell> response = [];
+
+        var type = myObject.GetType();
+        var properties = type.GetProperties();
+
+        foreach (var property in properties)
+        {
+            var attributes = property.GetCustomAttributes(typeof(CsvDataAttribute), true);
+
+
+            foreach(var attribute in attributes.Where(a => a is CsvDataAttribute))
+            {
+                var csvAttribute = attribute as CsvDataAttribute;
+                var propertyValue = property.GetValue(myObject, null);
+                var stringValue = propertyValue != null ?  propertyValue.ToString() : "";
+
+                response.Add(new CsvCell(csvAttribute!.GetName(), stringValue!));
+
+            }
+
+        }
+
+        return response;
+    }
+}

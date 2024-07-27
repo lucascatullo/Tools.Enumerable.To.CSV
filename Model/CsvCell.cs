48ebafb5f825f5cf952835ff11e2df7b6902f29a
LucasC <lucas@mymarketplacebuilder.com>
2024-07-27 14:48:58 +0200

    added the posibility to append inner properties to csv

--- Model/CsvCell.cs
+++ Model/CsvCell.cs
@@ -12,28 +12,42 @@ public class CsvCell(string name , string value)
 
     public static IList<CsvCell> ExtractLines(object myObject)
     {
-        IList<CsvCell> response = [];
+        List<CsvCell> response = [];
 
         var type = myObject.GetType();
         var properties = type.GetProperties();
 
         foreach (var property in properties)
         {
-            var attributes = property.GetCustomAttributes(typeof(CsvDataAttribute), true);
 
-
-            foreach(var attribute in attributes.Where(a => a is CsvDataAttribute))
+            var hasAppendInsideAttribute = property.GetCustomAttributes(typeof(AppendPropertiesToCsvAttribute), true).Any();
+            if (hasAppendInsideAttribute)
             {
-                var csvAttribute = attribute as CsvDataAttribute;
+                var innerObject = property.GetValue(myObject, null);
+                if (innerObject != null)
+                    foreach (CsvCell cell in ExtractLines(innerObject))
+                        response.Add(cell);
+            }
+            else
+            {
+                var attributes = property.GetCustomAttributes(typeof(CsvDataAttribute), true);
+
+                if (attributes.Length == 0) continue;
+
+                var csvAttribute = attributes.First() as CsvDataAttribute;
                 var propertyValue = property.GetValue(myObject, null);
-                var stringValue = propertyValue != null ?  propertyValue.ToString() : "";
+                var stringValue = propertyValue != null ? propertyValue.ToString() : "";
 
                 response.Add(new CsvCell(csvAttribute!.GetName(), stringValue!));
-
             }
-
         }
 
+        if (AreCellNamesRepeated(response))
+            throw new Exception("Names with the CSV Attribute must be unique. (Check for inneer properties)");  
         return response;
     }
+
+
+    private static bool AreCellNamesRepeated(IList<CsvCell> cells) => cells.Count != cells.DistinctBy(c => c.Name).Count();
+
 }
